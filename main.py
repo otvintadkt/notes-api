84cddc1fca2c450dab37bccca679690ac5b2ba3f
otvintadkt <skvandrew303@gmail.com>
2026-08-08 16:59:39 +0400

    More functionality added

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from typing import Any, Optional
 from datetime import datetime
 from fastapi import FastAPI, Path
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import sqlite3
 from config import *
 
 app = FastAPI()
 
-
 with sqlite3.connect(NOTES_DB_NAME) as connection:
-    cursor = connection.cursor()
-    cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        content TEXT
-    );
-    """)
+	cursor = connection.cursor()
+	cursor.execute(
+		"""
+		CREATE TABLE IF NOT EXISTS notes (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			date_posted DATE,
+			content TEXT
+		);
+		""")
 
 """
 get - get information
@@ -26,33 +27,61 @@ put - update
 delete - delete smth
 """
 
+
 class BaseNote(BaseModel):
-    name: str
-    content: str
-    date_posted: datetime
+	name: str
+	content: str
+	# date_posted: datetime
+
 
 @app.get("/")
-async def read_notes():
-    with sqlite3.connect(NOTES_DB_NAME) as connection:
-        cursor = connection.cursor()
-        notes = cursor.execute(
-        """
-        SELECT * FROM notes;
-        """).fetchall()
-        return notes
+def get_all_notes():
+	with sqlite3.connect(NOTES_DB_NAME) as connection:
+		cursor = connection.cursor()
+		notes = cursor.execute(
+			"""
+			SELECT * FROM notes;
+			""").fetchall()
+		return notes
+
 
 @app.post("/")
-async def post_note(note: BaseNote):
-    with sqlite3.connect(NOTES_DB_NAME) as connection:
-        cursor = connection.cursor()
-        cursor.execute(
-        """
-        INSERT INTO notes (name, date_posted, content)
-        VALUES (?, ?, ?)
-        """,
-        (note.name, datetime.now().isoformat(), note.content)
-        )
+def post_note(note: BaseNote):
+	with sqlite3.connect(NOTES_DB_NAME) as connection:
+		cursor = connection.cursor()
+		try:
+			cursor.execute("""
+            INSERT INTO notes (name, date_posted, content)
+            VALUES (?, ?, ?)
+            """,
+			               (note.name, datetime.now().isoformat(), note.content)
+			               )
+			new_id = cursor.lastrowid
+			return {"Success, note id": new_id}
+		except:
+			return {"Fail"}
 
-        new_id = cursor.lastrowid
-        return {"Success, note id": new_id}
-    return {"Fail": ""}
+
+@app.get("/{note_id}")
+def get_note_by_id(note_id: int):
+	with sqlite3.connect(NOTES_DB_NAME) as connection:
+		cursor = connection.cursor()
+		note = cursor.execute("""
+            SELECT * FROM notes WHERE id = ?
+        """, (note_id,)).fetchall()
+		if len(note) == 0:
+			return {"Note with this id doesn't exist"}
+		else:
+			return note
+
+
+def delete_note_by_id(note_id: int):
+	with sqlite3.connect(NOTES_DB_NAME) as connection:
+		cursor = connection.cursor()
+		cursor.execute("""
+            DELETE FROM notes WHERE id = ?
+        """, (note_id,))
+		if cursor.rowcount > 0:
+			return {f"Successfully deleted note with id {note_id}"}
+		else:
+			return {f"Couldn't delete note with id {note_id}"}
